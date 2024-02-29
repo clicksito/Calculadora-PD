cc818fa890e6f68833a42af8abf7cec9fe49cf90
Antonio <suazoantonio67@gmail.com>
2024-02-29 00:07:39 -0300

    creado el archivo principal
    agregadas 2 variables y el titulo

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+# Titulo
+print("Calculadora Ponderaciones")
+
+# Variables
+# Universidades
+universidad = input("Ingrese Su Universidad (UDEC, UBB, UCH, USACH, PUC): ")
+# Carrera
+carrera = input("Escoja su carrera (Informatica, Industrial, Medicina): ")
+# Porcentajes
