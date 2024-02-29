75cbd4b2de739774723c6682fbfc7005ac35c421
Antonio <suazoantonio67@gmail.com>
2024-02-29 00:16:34 -0300

    agregados primeros valores a pedir
    nem, m1, m2, lect

--- calculadora.py
+++ calculadora.py
@@ -7,3 +7,7 @@ universidad = input("Ingrese Su Universidad (UDEC, UBB, UCH, USACH, PUC): ")
 # Carrera
 carrera = input("Escoja su carrera (Informatica, Industrial, Medicina): ")
 # Porcentajes
+nem = input("Ponga su puntaje NEM: ")
+m1 = input("Ponga su puntaje de M1: ")
+m2 = input("Ponga su puntaje de M2: ")
+lect = input("Ponga su puntaje de Competencia lectora")
