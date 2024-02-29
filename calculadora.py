2d34ed0d65205b6270c767dd9b8b4a443f4cb6e5
Antonio <suazoantonio67@gmail.com>
2024-02-29 14:27:07 -0300

    agregada la opcion de carrera
    ahora puedes elegir que carrera(solo informatica por ahora)

--- calculadora.py
+++ calculadora.py
@@ -4,21 +4,26 @@ print("Calculadora Ponderaciones")
 # Variables
 # Universidades
 universidad = input("Ingrese Su Universidad (UDEC, UBB, UCH, USACH, PUC): ")
-# Carrera
-carrera = input("Escoja su carrera (Informatica, Industrial, Medicina): ")
 # Porcentajes
-nem = input("Ponga su puntaje NEM: ")
-ranking = input("Ponga su puntaje Ranking: ")
-m1 = input("Ponga su puntaje de M1: ")
-m2 = input("Ponga su puntaje de M2: ")
-lect = input("Ponga su puntaje de Competencia lectora: ")
-history = input("Ponga su puntaje de Historia: ")
-ciencias = input("Ponga su puntaje de Ciencias: ")
+nem = int(input("Ponga su puntaje NEM: "))
+ranking = int(input("Ponga su puntaje Ranking: "))
+m1 = int(input("Ponga su puntaje de M1: "))
+m2 = int(input("Ponga su puntaje de M2: "))
+lect = int(input("Ponga su puntaje de Competencia lectora: "))
+history = int(input("Ponga su puntaje de Historia: "))
+ciencias = int(input("Ponga su puntaje de Ciencias: "))
 
 if universidad == "UDEC":
-    nemudecinf = nem * 20
-    rankingudecinf = ranking * 15
-    m1udecinf = m1 * 25
-    m2udecinf = m2 * 15
-    lectudecinf = lect * 15
+    nemudecinf = nem * 0.20
+    rankingudecinf = ranking * 0.15
+    m1udecinf = m1 * 0.25
+    m2udecinf = m2 * 0.15
+    lectudecinf = lect * 0.15
     historyudecinf = history * 0
+    cienciasudecinf = ciencias * 0.10
+
+totaludecinf = nemudecinf + rankingudecinf + m1udecinf + \
+    m2udecinf + lectudecinf + historyudecinf + cienciasudecinf
+carrera = input("Escoja su carrera (Informatica, Industrial, Medicina): ")
+if carrera == "Informatica":
+    print(totaludecinf)
