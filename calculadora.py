ca3aa71f03a8bee4680683e7c7d93c50196296d5
Antonio <suazoantonio67@gmail.com>
2024-02-29 12:09:12 -0300

    iniciada la ponderacion UDEC
    agregado varias variables

--- calculadora.py
+++ calculadora.py
@@ -8,6 +8,17 @@ universidad = input("Ingrese Su Universidad (UDEC, UBB, UCH, USACH, PUC): ")
 carrera = input("Escoja su carrera (Informatica, Industrial, Medicina): ")
 # Porcentajes
 nem = input("Ponga su puntaje NEM: ")
+ranking = input("Ponga su puntaje Ranking: ")
 m1 = input("Ponga su puntaje de M1: ")
 m2 = input("Ponga su puntaje de M2: ")
-lect = input("Ponga su puntaje de Competencia lectora")
+lect = input("Ponga su puntaje de Competencia lectora: ")
+history = input("Ponga su puntaje de Historia: ")
+ciencias = input("Ponga su puntaje de Ciencias: ")
+
+if universidad == "UDEC":
+    nemudecinf = nem * 20
+    rankingudecinf = ranking * 15
+    m1udecinf = m1 * 25
+    m2udecinf = m2 * 15
+    lectudecinf = lect * 15
+    historyudecinf = history * 0
